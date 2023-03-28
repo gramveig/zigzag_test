e8b33487b863355cb282596fa671045ae9eb5dc0
Alexey Vlasyuk <mail@i-alex.net>
2023-03-28 16:15:11 +0300

    Generate home yard

--- Assets/Scripts/Views/Road.cs
+++ Assets/Scripts/Views/Road.cs
@@ -9,7 +9,23 @@ namespace Alexey.ZigzagTest.Views
 
         public void GenerateHomeYard()
         {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
 
+                    InitRoadBlock(x, y);
+                }
+            }
+        }
+
+        private void InitRoadBlock(int x, int y)
+        {
+            Instantiate(_roadBlock, new Vector3(x, 0, y), Quaternion.identity);
         }
     }
 }
